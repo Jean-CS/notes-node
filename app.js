6f260eb6d8bf2ece36aabd2d0068b8840096a6cd
Jean Santos <jeansantosc@gmail.com>
2018-07-06 18:33:39 -0300

    user input with yargs module

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ console.log('starting app');
 // built-in node modules
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 
 const notes = require('./notes');
 
+const argv = yargs.argv;
+
 // node app.js 'command'
 var command = process.argv[2];
-console.log("Command:" + command);
+console.log("Command:", process.argv);
+console.log('Yargs: ', argv);
 
 if (command === "add") {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes');
+  notes.getAll();
 } else if (command === 'read') {
-  console.log('Reading note');
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing note');
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,7 +1,24 @@
 console.log('starting notes.js');
 
-// module is an object
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'new note';
-}
+let addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+let getAll = () => {
+  console.log('Getting all notes');
+};
+
+let getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+let removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote, //same as 'addNote: addNote'
+  getAll,
+  getNote,
+  removeNote
+};
