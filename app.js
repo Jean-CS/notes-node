c640e1e338049887b71cbabc656a8eb7befdb0d1
Jean Santos <jeansantosc@gmail.com>
2018-07-06 00:25:52 -0300

    lodash sample functions

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ console.log('starting app');
 // built-in node modules
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-let res = notes.addNum(2, 3);
-console.log(res);
+console.log(_.isString(true)); // false
+console.log(_.isString('Jean')); // true
+
+// removes any duplicates
+let filteredArray = _.uniq(['Jean', 1, 'Jean', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//let res = notes.addNum(2, 3);
+//console.log(res);
 
 //let user = os.userInfo();
 
