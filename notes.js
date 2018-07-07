61766267eeb69499ab30a6fef581908b1ea4502b
Jean Santos <jeansantosc@gmail.com>
2018-07-07 15:57:54 -0300

    getNote functionality

--- notes.js
+++ notes.js
@@ -36,7 +36,9 @@ let getAll = () => {
 };
 
 let getNote = (title) => {
-  console.log('Getting note', title);
+  let notes = fetchNotes();
+  let filteredNote = notes.filter((note) => note.title === title);
+  return filteredNote[0];
 };
 
 let removeNote = (title) => {
