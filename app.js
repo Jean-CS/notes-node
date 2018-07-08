c4e12ebf64251a765b4c9af78ff7b8b3fdb7b25c
Jean Santos <jeansantosc@gmail.com>
2018-07-08 14:39:17 -0300

    list all notes functionality

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ if (command === "add") {
     console.log('Duplicate note');
   }
 } else if (command === 'list') {
-  notes.getAll();
+  let allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach(note => notes.logNote(note));
 } else if (command === 'read') {
   let note = notes.getNote(argv.title);
   if (note) {
