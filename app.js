9f1799dcaaf66e9dab72aefec91eab5cc1163ad7
Jean Santos <jeansantosc@gmail.com>
2018-07-08 15:17:45 -0300

    refactoring command options

--- app.js
+++ app.js
@@ -5,33 +5,28 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
 const argv = yargs
   .command('add', 'Add a new note', {
-    title: {
-      describe: 'Title of note',
-      demand: true,
-      alias: 't'
-    },
-    body: {
-      describe: 'Body of note',
-      demand: true,
-      alias: 'b'
-    }
+    title: titleOptions,
+    body: bodyOptions
   })
   .command('list', 'List all notes')
   .command('read', 'Read a note', {
-    title: {
-      describe: 'Title of note',
-      demand: true,
-      alias: 't'
-    },
+    title: titleOptions,
   })
   .command('remove', 'Remove a note', {
-    title: {
-      describe: 'Title of note',
-      demand: true,
-      alias: 't'
-    },
+    title: titleOptions,
   })
   .help()
   .argv;
